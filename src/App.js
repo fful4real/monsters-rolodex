1066b05aadfdb79c0a52baff5c56fae15ca7ec87
Felix FUL CHI <fful4real@yahoo.com>
2019-11-22 23:42:30 +0400

    Final Commit for today

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css'
+
 import { CardList } from './components/card-list/card-list.component'
 import { SearchBox } from './components/search-box/search-box.component'
 
