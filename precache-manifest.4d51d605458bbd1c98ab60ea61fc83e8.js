1a2a97e1bef07861b9874e7722638f039f134763
Felix FUL CHI <fful4real@yahoo.com>
2019-11-22 23:39:24 +0400

    Updates

---
+++ precache-manifest.4d51d605458bbd1c98ab60ea61fc83e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d17cc808b1fdebf8b8332baf12552be",
+    "url": "/fful4real/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c12db264dd583383dae9",
+    "url": "/fful4real/monsters-rolodex/static/css/main.897964b0.chunk.css"
+  },
+  {
+    "revision": "1dc917cb9cf2c038309f",
+    "url": "/fful4real/monsters-rolodex/static/js/2.5404069b.chunk.js"
+  },
+  {
+    "revision": "c12db264dd583383dae9",
+    "url": "/fful4real/monsters-rolodex/static/js/main.d689343b.chunk.js"
+  },
+  {
+    "revision": "def2a7bce197cd26d5ec",
+    "url": "/fful4real/monsters-rolodex/static/js/runtime-main.8461450f.js"
+  }
+]);
